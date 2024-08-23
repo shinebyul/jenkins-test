3aa3b0e81bfffa299c03c0b64de49aab31ad810a
shinebyul <qufdl8382@gmail.com>
2024-08-23 16:24:19 +0900

    [Init]초기

---
+++ src/main/java/com/example/jenkinsday01/JenkinsDay01Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsday01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDay01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDay01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsday01/JenkinsDay01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsday01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDay01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
